8d70a1209c2e68491b1ae0c3452b75a69b4cc332
Aaron Lain <aaron.j.lain@gmail.com>
2020-02-29 13:14:55 -0600

    buttons

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,6 +4,8 @@ import river from '../javascripts/components/river.js'
 
 const init = () => {
     $('#submit-new-bear').click(bear.newBearMaker);
+    $('body').on('click', '.tried-btn', triedEvent);
+    $('body').on('click', '.caught-btn', caughtEvent);
     river.newBearPrinter();
     console.log(bearData.bearArray)
 }
